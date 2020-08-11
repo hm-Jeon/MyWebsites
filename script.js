f554c7053e39ddb427d02e3c519ca0e2f042185b
hm-Jeon <69206253+hm-Jeon@users.noreply.github.com>
2020-08-11 22:01:48 +0900

    4.1

--- script.js
+++ script.js
@@ -1,43 +1,52 @@
 var body = {
   setColor: function(color) {
-    var target = document.querySelector('body');
-    target.style.color=color;
+    $('body').css('color', color);
+    $('body, h1, #websites').css('borderColor', color);
   },
   setBackgroundColor: function(color) {
-    var target = document.querySelector('body');
-    target.style.backgroundColor=color;
+    $('body').css('backgroundColor', color);
   }
 }
 
 var links = {
   setColor: function (color) {
-    var alist = document.querySelectorAll('a');
-    var i = 0;
-    while(i < alist.length){
-        alist[i].style.color = color;
-        i++;
-    }
+    $('a').css('color', color);
   }
 }
 
-function syncValue(mode) {
-  var tlist = document.querySelectorAll('.toggle');
-  for(i = 0; i < tlist.length; i++){
-    tlist[i].value = mode;
+var button = {
+  syncValue: function(mode) {
+    $('.toggle').val(mode);
+  },
+  hover: function(bgcolor, color) {
+    $(".toggle").hover(function(){
+      $(this).css("backgroundColor", bgcolor);
+      $(this).css("color", color);
+      if($('.toggle').val() == 'Day') {
+        $(this).css("borderColor", color);
+      }
+    },
+    function(){
+      $(this).css("background-color", color);
+      $(this).css("borderColor", bgcolor);
+      $(this).css("color", bgcolor);
+    });
   }
 }
 
 function nightDayHandler(self) {
-  if(self.value == '다크 모드') {
+  if(self.value == 'Night') {
     body.setColor('white');
     body.setBackgroundColor('black');
     links.setColor('white');
-    syncValue('기본 모드');
+    button.syncValue('Day');
+    button.hover('white', 'black');
   }
   else {  // self.value == '기본모드'
     body.setColor('black');
     body.setBackgroundColor('white');
     links.setColor('black');
-    syncValue('다크 모드');
+    button.syncValue('Night');
+    button.hover('black', 'white');
   }
 }
